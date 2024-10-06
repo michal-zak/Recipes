37e19094b1bca69e6867b684d25fc9a92b7f9fd4
Michal <michalp752@gmail.com>
2024-10-06 11:49:32 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by michal zak on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
